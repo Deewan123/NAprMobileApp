04b04f96387d4f5e2442ab721066598e8ceafb2a
Abhinav Singha <abhinavsingha@outlook.com>
2025-03-27 13:20:46 +0530

    first

---
+++ lib/api.dart
@@ -0,0 +1,48 @@
+const String ROOT="http://petregistration.mynoida.co.in/NaprNew/";
+// const String ROOT="https://56ce-203-153-42-234.ngrok.io/";
+// const String ROOT="http://192.168.100.238:3000/";
+const String ROOTDownload="http://petregistration.mynoida.co.in/webapi/";
+const String LOGIN="otpController/createOtpMobileApi";
+const String VERIFYOTP="otpController/verifyOtp";
+const String AllCategry="petCatMobile/getAllCategry";
+const String AppInfoAPI="otpController/appInformation";
+const String RegisterPet="mainForm/getAllApprovePet";
+const String CertificateRenew="mainForm/getVerifyStatus/";
+const String BreedType="breed/getBreedById/";
+const String ComplaintHistory="complaintApp/getComplaintWebApi";
+const String AllregisterPetStatus="mainForm/getAllPetPendingStatus";
+const String AllPetNeuteringStatus="mainForm/getAllPetNeutering";
+const String UpdateNeutering="mainForm/updateNeutering";
+const String DownloadPDf="mainForm/downloadForm";
+const String getAllVillage="addressVillage/getAllVillage";
+const String NewRegisterDocument="mainForm/uploadPhotoRegistrationApV2";
+const String NewDocumentUpload="mainForm/uploadPhotoRegistrationWithDocIdV2";
+const String AllSector="sector/getAllSector";
+const String AllScoicty="society/getAllScoicty";
+const String NewRegistrationForm="mainForm/createMainFormApiV2";
+const String UpdatePaymentInfo="mainForm/updatePaymentInfo";
+const String UpdatePaymentInfo1="mainForm/updatePaymentInfo1";
+const String AllPendingPet="mainForm/getAllPendingPet/";
+const String ClosePendingPet="mainForm/getClosePendingPetRequest/";
+const String ComplaintRegister="complaintApp/createComplaintWebApi";
+const String ComplaintDocument="complaintApp/uploadPhotoWebApi";
+const String AllPetStatusByFormId ="mainForm/getAllPetStatusByFormId/";
+const String updateNeutering ="mainForm/updateNeutering";
+const String RenewalPetData ="renewalForm/getRenewalData";
+const String RenewalMainForm ="renewalForm/renewalMainFormApiV2";
+const String UploadPhotoRenewal ="renewalForm/uploadPhotoRenewalApV2";
+const String VaccinationData ="vaccinationForm/getVaccinationData";  //pending vaccination
+const String AllDoctorList ="vaccinationForm/getAllDoctorList";
+const String VaccinationAppointment ="vaccinationForm/bookVaccinationAppointment";
+const String UserAppointment ="vaccinationForm/getUserAppointment";
+const String VaccinationSlot ="vaccinationForm/getVaccinationSlot";
+const String PendingVaccinationData ="vaccinationForm/getPendingVaccinationData";
+const String ReScheduleVaccinationAppointment ="vaccinationForm/reScheduleVaccinationAppointment";
+
+//Last working 8 Aug 2023
+const String Paymentrecept ="report/paymentrecept";
+const String SendEmail ="report/email";
+
+
+
+
